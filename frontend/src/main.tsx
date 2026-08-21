0af1811fe94cfb8c73b5f404b6a8b95a69c7cac5
Jonny <jonik0744clash@email.com>
2026-08-21 16:31:29 +0900

    Set up frontend app shell: Redux store, React Router 7, MUI theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router";
+import "./index.css";
+import { store } from "./app/store";
+import { router } from "./router";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
-)
+);
